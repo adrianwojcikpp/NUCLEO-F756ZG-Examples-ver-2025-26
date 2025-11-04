43c7052a4092c384d6d87837d21ce19dc2098fe4
Adrian Wójcik <54798218+adrianwojcikpp@users.noreply.github.com>
2025-11-04 21:43:27 +0100

    L06: SPI - Task #2 + #3

---
+++ L06_Example/Components/Inc/bmp280_config.h
@@ -0,0 +1,28 @@
+/**
+  ******************************************************************************
+  * @file    : bmp280_config.h
+  * @author  : AW       Adrian.Wojcik@put.poznan.pl
+  * @version : 2.5.0
+  * @date    : Oct 29, 2025
+  * @brief   : Configuration file for BMP2xx sensor driver library
+  *
+  ******************************************************************************
+  */
+#ifndef INC_BMP280_CONFIG_H_
+#define INC_BMP280_CONFIG_H_
+
+/* Public includes -----------------------------------------------------------*/
+#include "bmp280.h"
+
+/* Public typedef ------------------------------------------------------------*/
+
+/* Public define -------------------------------------------------------------*/
+
+/* Public macro --------------------------------------------------------------*/
+
+/* Public variables ----------------------------------------------------------*/
+extern struct bmp2_dev bmp2dev;
+
+/* Public function prototypes ------------------------------------------------*/
+
+#endif /* INC_BMP280_CONFIG_H_ */
